63bedaf93ab5bb796d304db66af0ac96f7f14365
script-js <134544514+script-js@users.noreply.github.com>
2024-04-06 08:44:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+var roomPicker = ["living","kitchen","family","bed1","bed2","bedbath"];
+var rooms = {
+  "living": {"room1":"kitchen","room2":"family","freindly":"Living Room"},
+  "kitchen": {"room1":"living","room2":"family","freindly":"Kitchen"},
+  "family": {"room1":"living","room2":"kitchen","freindly":"Family Room"},
+  "bath": {"room1":"bed2","room2":"bedbath","freindly":"Bathroom"},
+  "bed1": {"room1":"living","room2":"bed2","freindly":"Master Bedroom"},
+  "bed2": {"room1":"bed1","room2":"bedbath","freindly":"Bedroom"},
+  "bedbath": {"room1":"bed2","room2":"bath","freindly":"Ensuite Bathroom"}
+}
 
+var sourceR = roomPicker[Math.floor(Math.random() * roomPicker.length)];
+var currentRoom = "";
+
+function getRandCR() {
+  var src = rooms[sourceR]
+  var options = [sourceR,sourceR.room1,sourceR.room2]
+  currentRoom = options[Math.floor(Math.random() * options.length)]
+}
+getRandCR()
